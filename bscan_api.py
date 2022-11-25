3292d1c62b946ef7511f98869a36e98f7d29e6d4
sawyerdev <108633698+sawyerdev@users.noreply.github.com>
2022-11-24 23:08:59 -0300

    Update bscan_api.py
    Code rewritten using OOP!

--- bscan_api.py
+++ bscan_api.py
@@ -1,85 +1,76 @@
-import requests, json, datetime, sys
-import win32com.client as win32
+import os, requests, json, smtplib, ssl
 from time import sleep
+from dotenv import load_dotenv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-class RequestBscscan():
-    
-    def __init__(self, usdt_contract, wallet_address, api_key):
-        self.usdt_contract = usdt_contract
-        self.wallet_address = input(str('Digite a carteira da METAMASK: '))
+class Requester:
+    def __init__(self, token_contract, wallet_address, api_key):
+        self.token_contract = token_contract
+        self.wallet_address = wallet_address
         self.api_key = api_key
 
-    def json_load(self):
-        d = open('#PATH/FILE WITH INFORMATIONS: username (optional) and metamask wallet to verification')
-        self.dt = json.load(d)
-        return self.dt
-    
-    def requestApi(self):
+    def request_api(self):
         request = requests.get("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress={}&address={}&tag=latest&apikey={}".format(
-            self.usdt_contract, self.wallet_address, self.api_key))
+            self.token_contract, self.wallet_address, self.api_key))
 
         req_text = request.text
-        self.data = json.loads(req_text)
-    
-    def getTokenAmount(self):
-        small_token = float(self.data['result'])
-        self.token_amount = small_token/1000000000000000000
-        return self.token_amount
+        data = json.loads(req_text)
 
-request = RequestBscscan("0x55d398326f99059ff775485246999027b3197955","","API_KEY")
+        small_token = float(data['result'])
+        token_amount = small_token/1000000000000000000
+        return token_amount
 
-def setOutlookInstance():
-    global mailItem
-    olApp = win32.Dispatch('Outlook.Application')
-    olNS = olApp.GetNameSpace('MAPI')
-    mailItem = olApp.CreateItem(0)
+#Using USDT contract as example
+do_request = Requester("0x55d398326f99059ff775485246999027b3197955","WALLET_ADDRESS","BSCSCAN_API_KEY")
 
-def sendMail():
-    dt = request.json_load()
-    for i in dt['account']:
-        if request.wallet_address == i['metamask_wallet']:
-            username = i['username']
-   
-    mailItem.Subject = 'USDT RECEIVED!'
-    mailItem.BodyFormat = 1
-    mailItem.Body = f'{username} ACCOUNT PAYMENT HAS BEEN SUCCESSFULLY DEPOSITED!\n\nAMOUNT RECEIVED: ${amount_received} USDT'
-    mailItem.To = '' #email to send
+class TheProgram:
+    def __init__(self):
+        self.amount = []
+        self.amount_received = 0
+        
+    #Using Microsoft Outlook as example
+    def send_mail(self):
+        load_dotenv()
+        msg = MIMEMultipart()
+        msg['From'] = os.getenv("FROM_EMAIL")
+        password = os.getenv("PASSWORD")
+        msg['To'] = os.getenv("TO_EMAIL")
+        msg['Subject'] = 'USDT RECEIVED!'
+        message_content = f'ACCOUNT PAYMENT HAS BEEN SUCESSFULLY DEPOSITED!\n\nAMOUNT RECEIVED: ${self.amount_received}!'
 
-    try:
-        mailItem.Send()
-        print('Email successfully sent!\n')
-        sleep(3)
-    except Exception as err:
-        print(err)
-    
-def matchValues(now, last):
-    global amount_received
-    if now == last:
-        return "No amount received yet..."
-    elif now > last:
-        amount_received = now - last
-        print('\n======== READY TO SEND EMAIL AFTER NEW VALUE ALERT! ========\n')
-        sendMail()
-        amount.clear()
-    
-def main():
-    setOutlookInstance()
-    global amount
-    amount = []
-    print('\n==== STARTING PROGRAM =====\n')
-    while True:
-        request.requestApi()
-        now_value = request.getTokenAmount()
-        if len(amount) >= 2:
-            last_value = amount[-1]
-            res = matchValues(now_value, last_value)
-            print(res)
-            if len(amount) >= 100:
-                amount.clear()
-        else:
-            print(f"Total USDT: ${now_value}")
+        msg.attach(MIMEText(message_content, 'plain'))
 
-        amount.append(now_value)
-        sleep(1800)
+        server = smtplib.SMTP('smtp.office365.com: 587')
+        server.starttls()
 
-main()
+        server.login(msg['From'], password)
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    def match_values(self, now, last):
+        if now == last:
+            return "No new transactions..."
+        if now > last:
+            amount_received = now - last
+            print('\n======== READY TO SEND EMAIL AFTER NEW VALUE ALERT! ========\n')
+            self.send_mail()
+            self.amount.clear()
+
+    def main(self):
+        print('\n==== STARTING PROGRAM =====\n')
+        while True:
+            now_value = do_request.request_api()
+            if len(self.amount) >= 2:
+                last_value = self.amount[-1]
+                res = self.match_values(now_value, last_value)
+                print(res)
+                if len(self.amount) >= 100:
+                    self.amount.clear()
+            else:
+                print(f"Total USDT: ${now_value}")
+
+            self.amount.append(now_value)
+            sleep(1800) #checking every 30 minutes
+
+main = TheProgram()
+main.main()
